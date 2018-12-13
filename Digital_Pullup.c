1bd04ea90c98e21d3e247184065d815ce4bcbde6
Jenu Varghese Chacko <JenuC@users.noreply.github.com>
2018-12-13 13:36:43 -0600

    Update Digital_Pullup.c

--- Digital_Pullup.c
+++ Digital_Pullup.c
@@ -5,7 +5,8 @@ BASED ON EXAMPLE SKETCH @ http://www.arduino.cc/en/Tutorial/InputPullupSerial
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT_PULLUP);}
+  pinMode(2, INPUT_PULLUP);
+  pinMode(13, OUTPUT);}
 
 void loop() 
 {
