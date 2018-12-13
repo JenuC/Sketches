0ca002c9da3146ef47f9d12f218ac99c26ffc2d3
Jenu Varghese Chacko <JenuC@users.noreply.github.com>
2018-12-13 13:36:00 -0600

    Create Digital_Pullup.c

---
+++ Digital_Pullup.c
@@ -0,0 +1,18 @@
+/*
+TO READ DIG IN and TRIGGER PERIOD
+BASED ON EXAMPLE SKETCH @ http://www.arduino.cc/en/Tutorial/InputPullupSerial
+*/
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);}
+
+void loop() 
+{
+int sensorVal = digitalRead(2); // Keep in mind the pullup means the pushbutton's  
+                                // logic is inverted.
+if (sensorVal == HIGH)  {digitalWrite(13, LOW); } 
+else                    {digitalWrite(13, HIGH); }
+}
+
+
