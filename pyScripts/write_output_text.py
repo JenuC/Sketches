c53206dacb5f796ef0fdf7d441709b441c63a68c
Jenu Varghese Chacko <JenuC@users.noreply.github.com>
2018-12-13 13:51:28 -0600

    Create write_output_text.py

---
+++ pyScripts/write_output_text.py
@@ -0,0 +1,44 @@
+import serial
+import time
+from datetime import datetime
+
+serial_port = u'COM4' # '/dev/ttyACM4';
+baud_rate = 115200;   #In arduino, Serial.begin(baud_rate)
+write_to_file_path = "outputls2.txt";
+output_file = open(write_to_file_path, "w+");
+ser = serial.Serial(serial_port, baud_rate)
+
+frame_number=0
+
+data='0'
+print(ser);
+st=time.time_ns()
+stopcycle=False
+while (~stopcycle):
+    #line = ser.readline();
+    #line = line.decode("utf-8") #ser.readline returns a binary, convert to string
+    #print(line);
+    #output_file.write(line);
+    data= ser.read()
+    #data = ser.read().decode("utf-8");
+    #print (data)
+    if data==b'X' :
+        #ms= int(round(time.time() * 1000)) #ms
+        ms= time.time_ns()
+        ms=ms-st
+        #ms=ms/1000
+        #ms=datetime.now()
+        #print(ms)
+        #ms=ms.microsecond
+        output_file.write(str(ms)+"\n");
+        frame_number+=1
+        if frame_number%(1000) ==0:
+            print ("Frames Measured = ",frame_number)
+        if frame_number ==2000:
+            #    stopcycle=True
+            output_file.close()
+            break
+        
+import numpy
+data= numpy.loadtxt('outputls2.txt')
+print (numpy.ediff1d(data).mean())
